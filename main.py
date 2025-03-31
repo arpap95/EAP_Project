f68f5d5b02c2d29c0046b4d119924dde1a9cd0a6
arpap95 <166176746+arpap95@users.noreply.github.com>
2025-03-31 20:49:02 +0300

    bug fixes

--- main.py
+++ main.py
@@ -9,20 +9,3 @@ if __name__ == "__main__":
     app = App(root)
 
     root.mainloop()
-
-import ttkbootstrap as ttk
-import tkinter as tk
-from gui_components.menu import show_main_menu
-
-
-root = tk.Tk()
-root.title("Διαχείριση Ραντεβού")
-root.geometry("800x500")
-
-content_frame = ttk.Frame(root)
-content_frame.pack(fill="both", expand=True)
-
-show_main_menu(content_frame)
-
-root.mainloop()
-
